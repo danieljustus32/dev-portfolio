6e1b5c677c798281738c5a9493b08956347d103d
Daniel Justus <daniel.justus32@gmail.com>
2023-03-04 13:18:31 -0800

    Make header fixed, add scale transition

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -7,11 +7,45 @@ import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 
+import { useState, useEffect } from 'react'
+
 const LayoutWrapper = ({ children }) => {
+  // Detect page scroll to shrink header, see https://stackoverflow.com/questions/68087971/get-scrollbar-position-with-nextjs,
+  // useEffect now only runs on the client so no need to detect if window != null
+  const [scrollY, setScrollY] = useState(0)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY)
+    };
+
+    window.addEventListener("scroll", handleScroll)
+
+        // just trigger this so that the initial state 
+    // is updated as soon as the component is mounted
+    // related: https://stackoverflow.com/a/63408216
+    handleScroll();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [scrollY])
+
+  useEffect(() => {
+    if (scrollY > 200) {
+      document.getElementById("navbar").classList.add("lg:scale-75")
+    }
+    else {
+      document.getElementById("navbar").classList.remove("lg:scale-75")
+    }
+  }, [scrollY])
+
   return (
     <SectionContainer>
       <div className="flex h-screen flex-col justify-between">
-        <header className="flex items-center justify-between py-10">
+        <header className="fixed z-50 left-0 w-screen px-12 flex items-center justify-between py-10 w-1/2 transition-all" id="navbar">
           <div>
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
@@ -44,7 +78,7 @@ const LayoutWrapper = ({ children }) => {
             <MobileNav />
           </div>
         </header>
-        <main className="mb-auto">{children}</main>
+        <main className="mb-auto mt-36">{children}</main>
         <Footer />
       </div>
     </SectionContainer>
