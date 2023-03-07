6b51d3a0c5681a4c655e87688b3faa9d9ab8cfce
Daniel Justus <daniel.justus32@gmail.com>
2023-03-06 20:01:07 -0800

    Update headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,5 +1,4 @@
 const headerNavLinks = [
-  { href: '/latest', title: 'Latest'},
   { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
